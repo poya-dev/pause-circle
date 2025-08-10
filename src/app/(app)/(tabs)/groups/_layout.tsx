c4a06f54c3c8c4dfdaeeedb1c43d8f75267bc065
poya-dev <poya.kpu.2020@gmail.com>
2025-08-09 19:34:42 -0700

    refactor: remove challenges layout/screen components, add groups layout and screen with new design

---
+++ src/app/(app)/(tabs)/groups/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from 'expo-router';
+import * as React from 'react';
+
+export default function GroupsLayout(): React.ReactElement {
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
